3221efc8cd8528a24719fe59210fb70626b22d12
Reece Williams <89943358+Tisreece@users.noreply.github.com>
2023-08-20 17:24:51 +0100

    Update AbilityManager.cs
    Added dropdowns to the public variables on the AbilityManager

--- Bible_Bash/Assets/Abilities/AbilityManager.cs
+++ Bible_Bash/Assets/Abilities/AbilityManager.cs
@@ -2,7 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
+#if UNITY_EDITOR
+[CustomEditor(typeof(AbilityManager))]
+class AbilityManagerEditor : Editor
+{
+    bool ShowAbilities = true;
+    bool ShowAbilitiesToEquip = true;
+    bool ShowMisc = true;
+
+    public override void OnInspectorGUI()
+    {
+        GUIStyle Foldout = new GUIStyle(GUI.skin.label);
+        Foldout.richText = true;
+
+        ShowAbilities = EditorGUILayout.Foldout(ShowAbilities, "<b><size=14>Abilities</size></b>", true, Foldout);
+        if (ShowAbilities)
+        {
+            EditorGUI.indentLevel++;
+            ShowAbilitiesToEquip = EditorGUILayout.Foldout(ShowAbilitiesToEquip, "<b>Abilities Equipped At Start</b>", true, Foldout);
+            if (ShowAbilitiesToEquip)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("SecondarySlot"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("Ability1Slot"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("Ability2Slot"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("CrucifixionSlot"));
+                EditorGUI.indentLevel--;
+            }
+
+            ShowMisc = EditorGUILayout.Foldout(ShowMisc, "<b>Misc</b>", true, Foldout);
+            if (ShowMisc)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("AbilityData"));
+                EditorGUI.indentLevel--;
+            }
+            EditorGUI.indentLevel--;
+        }
+    }
+}
+#endif
 public class AbilityManager : MonoBehaviour
 {
     public GameObject PlayerCharacter;
